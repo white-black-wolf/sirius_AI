97f5a264610128500db59ec6c0f9a61ce58e80fc
wbw <151435718+white-black-wolf@users.noreply.github.com>
2024-11-03 17:24:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import sqlite3 as db
 from sklearn.svm import SVC
+import spacy
 
 #расшифровка значений
 dictt = {
@@ -26,6 +27,8 @@ dictt = {
     15: "Розыгрыш",
 }
 
+
+
 #коннектим датасет
 conn = db.connect('post_types_copy.db')
 cursor = conn.cursor()
@@ -35,21 +38,41 @@ conn.commit()
 # получаем значения из датасета
 conn.row_factory = lambda cursor, row: row[0]
 c = conn.cursor()
-texts = c.execute('SELECT text FROM posts').fetchall()
+textss = c.execute('SELECT text FROM posts').fetchall()
 y = c.execute('SELECT post_type FROM posts').fetchall()
 
-#получаем ветор
+#загружаем обученный конвейер для русского языка
+nlp = spacy.load("ru_core_news_lg")
+
+texts = []
+stop_words = nlp.Defaults.stop_words
+custom_stop_words = ["№", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", "\"", "\\", "|", ",", ".", "<", ">", "/", "?", "~", "`"]
+stop_words.update(custom_stop_words)
+#очитска, нормализация и токенизация текста
+for i in textss:
+    s = ''
+    doc = nlp(i)
+    for token in doc:
+        if not token.is_punct and not token.is_stop and not str(token).isdigit():
+            s+=token.lemma_
+            s+=' '
+    texts.append(s)
+    
+
+
+#векторизация
 vectorizer = CountVectorizer()#иниацилизация
 X = vectorizer.fit_transform(texts)
 x = X.toarray()
 labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]  # Пример меток классов
 
-X_train, X_test, y_train , y_test = x[0:975], x[975:], y[0:975], y[975:]
+X_train, X_test, y_train , y_test = x[0:1200], x[1200:], y[0:1200], y[1200:]
 
 
 nb_classifier = MultinomialNB()#инициализация
 nb_classifier.fit(X_train, y_train)#обучаем модель
 y_pred = nb_classifier.predict(X_test)#получаем ответ от нейросети
+print(f"Точность: {accuracy_score(y_test, y_pred)}")
 di = int(y_pred[-1])#обрабатываем ответ
 print(dictt.get(di))#выводим ответ
 
